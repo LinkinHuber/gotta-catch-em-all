c4ed7462d01f7a6f6e680a0bd6572a3cab2e344b
Jason Triemert <jaytriemert@gmail.com>
2023-04-12 12:51:16 -0500

    added pokemon api

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
-getPokemon();
+fetchPokemon();
 
-async function getPokemon(){
+async function fetchPokemon(){
   const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
   const data = await response.json()
   console.log(data)
 }
+
+selectPokemon ();
+
+
+
+
