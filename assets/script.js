f7a8e99d9b285d6fdb518da38a5d085361ea7465
Jason Triemert <jaytriemert@gmail.com>
2023-04-13 10:57:00 -0500

    Api searchable by pokemon array

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,23 @@
-fetchPokemon();
+var pokemon = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise"];
+var pokemonName;
+var num;
+var pokemonSearch = "squirtle"
 
+console.log(pokemon.length);
+
+for(var i=0; i < pokemon.length; i++) {
+  console.log("Great to see you, " + pokemon[i] + "!")
+  if (pokemon[i] === pokemonSearch){
+    var num = i+1
+    fetchPokemon();
+  };
+}
 async function fetchPokemon(){
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
-  const data = await response.json()
+  // num++;
+  var api = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+  var response = await fetch(api);
+  var data = await response.json();
+  pokemonName = data.name;
+  console.log(pokemonName);
   console.log(data)
 }
-
-
-  
-
-
