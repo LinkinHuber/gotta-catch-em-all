16a2a9bb0ee4f7fc6a04733be3a014406cb4f98a
Lucas Lippert <lippertnegenl@gmail.com>
2023-04-13 22:55:50 -0500

    js opens new webpage

--- assets/search-script.js
+++ assets/search-script.js
@@ -1,6 +1,11 @@
 var pokemonName;
 var pokemonSearch = "";
 
+document.getElementById('search-btn2').addEventListener('click', function(event) {
+  event.preventDefault();
+  pokemonSearch = document.querySelector("input").value;
+  fetchPokemon();
+});
   
 async function fetchPokemon(){
   var api = "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch
@@ -10,19 +15,23 @@ async function fetchPokemon(){
   var pokemonName = data.name;
   var pokemonWeight = data.weight;
   var pokemonHeight = data.height;
+  var pokemonAbilities0 = data.abilities[0].ability.name;
+  var pokemonAbilities1 = data.abilities[1].ability.name;
+ 
+    
   console.log(pokemonName);
   console.log(pokemonWeight);
   console.log(pokemonHeight);
   console.log(urlArtwork);
+  console.log(pokemonAbilities0);
+  console.log(pokemonAbilities1);
   console.log(data);
 }
+/*
+function displayPokemon(data) {
+  var pokemonAbilities = data.abilities;
+}
 
-var queryString = "./secondpageindex.html";
+displayPokemon();
 
-
-document.getElementById('search-btn1').addEventListener('click', function(event) {
-  event.preventDefault()
-  pokemonSearch = document.querySelector("input").value
-  fetchPokemon()
-  location.assign(queryString);
-});
+*/
