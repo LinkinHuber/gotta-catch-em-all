d653047d0fad7c868cb48456fe862fef32e3087a
Jason Triemert <jaytriemert@gmail.com>
2023-04-14 13:42:57 -0500

    added displayPokemon function

--- assets/search-script.js
+++ assets/search-script.js
@@ -1,6 +1,7 @@
 var pokemonName;
 var pokemon;
 var pokemonSearch = "";
+var urlArtwork;
 
 document.getElementById('search-btn2').addEventListener('click', function(event) {
   event.preventDefault();
@@ -12,41 +13,18 @@ async function fetchPokemon(){
   var api = "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch
   var response = await fetch(api);
   var data = await response.json();
-  var urlArtwork = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + data.id + ".png";
-  var pokemon = data;
-  // console.log(pokemon.name);
-  // console.log(pokemon.weight);
-  // console.log(pokemon.height);
-  // console.log(urlArtwork);
-  // console.log(data);
-  var pokemonAbilities0 = data.abilities[0].ability.name;
-  // var pokemonAbilities1 = data.abilities[1].ability.name;
-  
-  var pokemonStats = {
-    name: pokemon.name,
-    height: pokemon.height,
-    weight: pokemon.weight,
-    abilities: [data.abilities[0].ability.name, data.abilities[1].ability.name]
-  };
-  
-  // console.log(urlArtwork);
-  console.log(pokemonAbilities0);
-  // console.log(pokemonAbilities1);
-  console.log(pokemonStats);
+  urlArtwork = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + data.id + ".png";
+  pokemon = data;
+  console.log(data);
+  displayPokemon();
+
+}
+function displayPokemon(){
   document.getElementById("result-name").innerHTML = ("Name:" + " " + pokemon.name);
   document.getElementById("result-height").innerHTML = ("Height:" + " " + pokemon.height);
   document.getElementById("result-weight").innerHTML = ("Weight:" + " " + pokemon.weight);
-  document.getElementById("result-id").innerHTML = ("Id:" + " " + pokemon.id);
-
-  
+  document.getElementById("result-abilities").innerHTML = ("Abilities:" + " " + pokemon.abilities[0].ability.name + ", " + pokemon.abilities[1].ability.name);
+  document.getElementById("result-type").innerHTML = ("Type:" + " " + pokemon.types[0].type.name + ", " + pokemon.types[1].type.name);
   var pic =  document.getElementById("result-img1")
   pic.src = urlArtwork;
 }
-/*
-function displayPokemon(data) {
-  var pokemonAbilities = data.abilities;
-}
-
-displayPokemon();
-
-*/
