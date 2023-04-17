425eb804d60c7dbb7b2b39455eb2cd6a50655b1e
Jason Triemert <jaytriemert@gmail.com>
2023-04-17 14:26:32 -0500

    added audio to first page

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 var pokemonName;
 var pokemonSearch = "";
 
-
+function aud_play_pause() {
+  var myAudio = document.getElementById("myAudio");
+  console.log(myAudio)
+    myAudio.play();
+}
 async function fetchPokemon() {
   var api = "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch;
   var response = await fetch(api);
@@ -21,12 +25,17 @@ var queryString = "./secondpageindex.html";
 
 document.getElementById("search-btn").addEventListener("click", function(event) {
   event.preventDefault();
+  aud_play_pause();
   pokemonSearch = document.querySelector("input").value;
   // Validation and formatting (how are we interacting with weird cases, i.e., Mr. Mime)
   localStorage.setItem('pokemonSearch', JSON.stringify(pokemonSearch));
   var queryString = `./secondpageindex.html?pokemon=${pokemonSearch}`; 
   console.log(queryString)
-  location.assign(queryString);
+  setTimeout(()=> {
+    location.assign(queryString.toLowerCase())
+  }, 1100)
+
+  // location.assign(queryString);
 
   // fetchPokemon(pokemonSearch).then(pokemon => {
   //   console.log(pokemon)
--- assets/search-script.js
+++ assets/search-script.js
@@ -9,7 +9,7 @@ console.log(initialSearch)
 
 document.getElementById("search-btn").addEventListener("click", function(event) {
   event.preventDefault();
-  pokemonSearch = document.querySelector("input").value;
+  pokemonSearch = document.querySelector("input").value.toLowerCase();
   aud_play_pause()
   fetchPokemon();
 });
@@ -56,4 +56,5 @@ function displayPokemon() {
   document.getElementById("result-type").innerHTML = (parseTypes(pokemon.types));
 }
 localStorage.setItem('pokemon', JSON.stringify(pokemon)); 
+
 fetchPokemon()
